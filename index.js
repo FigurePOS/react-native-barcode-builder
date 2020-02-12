519a4adfa3eaf0ba0bdce340641c87b650dc4eba
Jan Crha <jancrha@gmail.com>
2020-02-12 11:42:51 +0100

    Fix lib to use react-native-svg as peerDep

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import barcodes from 'jsbarcode/src/barcodes';
 
-import { Surface, Shape } from '@react-native-community/art';
+import Svg, { Path } from 'react-native-svg';
 
 export default class Barcode extends PureComponent {
   static propTypes = {
@@ -166,12 +166,18 @@ export default class Barcode extends PureComponent {
     };
     return (
       <View style={[styles.svgContainer, backgroundStyle]}>
-        <Surface height={this.props.height} width={this.state.barCodeWidth} visible={true}>
-          <Shape d={this.state.bars} fill={this.props.lineColor} visible={true} />
-        </Surface>
-        { typeof(this.props.text) != 'undefined' &&
-          <Text style={{color: this.props.textColor, width: this.state.barCodeWidth, textAlign: 'center'}} >{this.props.text}</Text>
-        }
+          <Svg
+            height={this.props.height}
+            width={this.props.width}
+            fill={this.props.lineColor}>
+            <Path
+              d={this.state.bars.join(" ")}/>
+          </Svg>
+          {typeof (this.props.text) != 'undefined' &&
+          <Text style={{ color: this.props.textColor, width: this.state.barCodeWidth, textAlign: 'center' }}>
+            {this.props.text}
+          </Text>
+          }
       </View>
     );
   }
